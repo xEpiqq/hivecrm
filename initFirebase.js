1900433ab5cb03e3c8444e26cd93f87cfc6326a1
xepiqq <jaydencrowther@gmail.com>
2024-07-10 13:42:20 -0600

    first commit

---
+++ initFirebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3yI7i1JQmWf6B1SBT-COQXs6399pj_78",
+  authDomain: "hive-92070.firebaseapp.com",
+  projectId: "hive-92070",
+  storageBucket: "hive-92070.appspot.com",
+  messagingSenderId: "431645767743",
+  appId: "1:431645767743:web:f25be3f653bc4a05f8e08a"
+};
+
+const app = initializeApp(firebaseConfig);
